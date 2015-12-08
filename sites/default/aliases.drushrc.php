c99416f3ca01e62517707bb501e0159130e5160c
Vasile Chindris <vasile.chindris@amazee.com>
2015-12-08 22:53:29 +0100

    Added the drush paths.

--- sites/default/aliases.drushrc.php
+++ sites/default/aliases.drushrc.php
@@ -4,4 +4,8 @@ $aliases['prod'] = array(
   'root' => '/var/www/chindris.net/html',
   'remote-host' => 'chindris.net',
   'remote-user' => 'vasi',
+  'path-aliases' => array(
+    '%drush' => '/home/vasi/.composer/vendor/bin',
+    '%drush-script' => '/home/vasi/.composer/vendor/bin/drush',
+   ),
 );
